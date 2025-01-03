40cf78da97d2f3e19778c2e4469cabda888c19ec
gallickgun <heraditya22@gmail.com>
2025-01-03 20:31:16 +0700

    working with list, returning a value from a function

--- main.go
+++ main.go
@@ -2,27 +2,30 @@ package main
 
 import "fmt"
 
-var (
-	taskOne   = "Learn Go"
-	taskTwo   = "Eat Dinner"
-	taskThree = "Read Email"
-)
-
-var taskItems = []string{taskOne, taskTwo, taskThree}
-
 func main() {
 
-	//categoryOne := "Private"
-	// categoryTwo := "Work"
+	var (
+		taskOne   = "Learn Go"
+		taskTwo   = "Eat Dinner"
+		taskThree = "Read Email"
+	)
+
+	var taskItems = []string{taskOne, taskTwo, taskThree}
 
 	fmt.Println("To Do List")
 
 	fmt.Println()
-	printTasks()
+	printTasks(taskItems)
+	taskItems = addTasks(taskItems, "Sleep")
 }
 
-func printTasks() {
+func printTasks(taskItems []string) {
 	for _, task := range taskItems {
 		fmt.Println(task)
 	}
+}
+
+func addTasks(taskItems []string, newTask string) []string {
+	var updatedTasks = append(taskItems, newTask)
+	return updatedTasks
 }
